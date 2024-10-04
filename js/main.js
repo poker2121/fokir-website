cdc3fa728b5d118c85930e14ca8a273e6971e76a
poker2121 <banoobnagy@gmail.com>
2024-10-04 13:35:49 +0300

    Add new project for Assignment 4

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".type", {
+  strings: ["Designer", "Developer"],
+  typeSpeed: 70,
+  backSpeed: 50,
+  loop: true,
+  startDelay: 300,
+  backDelay: 1400,
+  smartBackspace: false,
+});
